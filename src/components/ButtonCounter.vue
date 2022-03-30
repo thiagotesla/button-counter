e2ab3754cc555b7d62b86e06c8dc3c39fc663447
Thiago Tesla <thiagobarros95@gmail.com>
2022-03-30 16:03:25 -0300

    add logic to button counter component

---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  counting() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="count++">
+    This button was clicked {{ count }} times.
+  </button>
+</template>
