aca9daa315d8c2aa7756ae1f209856f7621a7370
Thiago Tesla <thiagobarros95@gmail.com>
2022-03-30 16:09:15 -0300

    add button counter to App as component

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import ButtonCounter from './components/ButtonCounter.vue'
+  
+export default {
+  components: {
+    ButtonCounter
+  }
+}
+</script>
+
+<template>
+	<h1>Here are many child components!</h1>
+	<ButtonCounter />
+	<ButtonCounter />
+	<ButtonCounter />
+</template>
